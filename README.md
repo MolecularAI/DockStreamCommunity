73ae3d783ca919102af776c8f70494595ec52dc7
Christian Margreitter <christian.margreitter@astrazeneca.com>
2021-08-02 14:50:25 +0200

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,38 @@
+# `DockStreamCommunity`
+
+## Description
+This repository is meant to store `Jupyter Notebook` tutorials and documentation for [DockStream](https://github.com/MolecularAI/DockStream). 
+
+## Supported Backends
+### Ligand Embedders
+* **[`RDKit`](https://www.rdkit.org/docs/GettingStartedInPython.html#working-with-3d-molecules)**
+* **[`Corina`](https://www.mn-am.com/products/corina)**
+* **[OpenEye's `OMEGA`](https://www.eyesopen.com/omega)**
+* **[Schrodinger's `LigPrep`](https://www.schrodinger.com/products/ligprep)**
+* **[`TautEnum`](https://github.com/OpenEye-Contrib/TautEnum/blob/master/README)**
+
+### Docking Backends
+* **[`AutoDock Vina`](http://vina.scripps.edu/index.html)**
+* **[`rDock`](http://rdock.sourceforge.net)**
+* **[OpenEye's `Hybrid`](https://www.eyesopen.com/oedocking-tk)**
+* **[Schrodinger's `Glide`](https://www.schrodinger.com/glide)**
+* **[CCDC's `GOLD`](https://www.ccdc.cam.ac.uk/solutions/csd-discovery/components/gold)**
+
+Note, that the `CCDC` package, the `OpenEye` toolkit and `Schrodinger`'s tools require you to obtain the respective software from those vendors.
+
+## Requirements
+A Conda environment is provided: `DockStreamCommunity` via `environment.yml` to execute the `Jupyter Notebook` tutorials. 
+Some notebooks also require the `DockStream` and `DockStreamFull` environments to be installed 
+(see [DockStream README](https://github.com/MolecularAI/DockStream)). 
+```
+git clone <DockStreamCommunity repository>
+cd <DockStreamCommunity directory>
+conda env create -f environment.yml
+conda activate DockStreamCommunity
+jupyter notebook
+```
+
+## Contributors
+Christian Margreitter (christian.margreitter@astrazeneca.com)
+Jeff Guo (jeff.guo@astrazeneca.com)
+Alexey Voronov (alexey.voronov1@astrazeneca.com)
---
+++ notebooks/conf.py
@@ -0,0 +1,115 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+master_doc = 'index'
+
+project = 'DockStream'
+copyright = '2021, Christian Margreitter'
+author = 'Christian Margreitter'
+
+# The full version, including alpha/beta/rc tags
+release = "1.0.0"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "nbsphinx",
+    "sphinx.ext.mathjax" # for math equations
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+exclude_patterns = ['_build']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for LaTeX output ---------------------------------------------
+
+# See https://www.sphinx-doc.org/en/master/latex.html
+latex_elements = {
+    'papersize': 'a4paper',
+    'printindex': '',
+    'sphinxsetup': r"""
+        %verbatimwithframe=false,
+        %verbatimwrapslines=false,
+        %verbatimhintsturnover=false,
+        VerbatimColor={HTML}{F5F5F5},
+        VerbatimBorderColor={HTML}{E0E0E0},
+        noteBorderColor={HTML}{E0E0E0},
+        noteborder=1.5pt,
+        warningBorderColor={HTML}{E0E0E0},
+        warningborder=1.5pt,
+        warningBgColor={HTML}{FBFBFB},
+    """,
+    'preamble': r"""
+\usepackage[sc,osf]{mathpazo}
+\linespread{1.05}  % see http://www.tug.dk/FontCatalogue/urwpalladio/
+\renewcommand{\sfdefault}{pplj}  % Palatino instead of sans serif
+\IfFileExists{zlmtt.sty}{
+    \usepackage[light,scaled=1.05]{zlmtt}  % light typewriter font from lmodern
+}{
+    \renewcommand{\ttdefault}{lmtt}  % typewriter font from lmodern
+}
+\usepackage{booktabs}  % for Pandas dataframes
+""",
+}
+
+latex_documents = [
+    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
+]
+
+latex_show_urls = 'footnote'
+latex_show_pagerefs = True
+
+# -- Options for EPUB output ----------------------------------------------
+
+# These are just defined to avoid Sphinx warnings related to EPUB:
+version = release
+suppress_warnings = [
+    'nbsphinx',
+    'epub.unknown_project_files']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_with_keys': True,
+    'collapse_navigation': False,
+}
+html_title = project + ' version ' + release
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = [
+    "modifications.css"
+]
